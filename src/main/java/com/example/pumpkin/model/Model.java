b7570c6c4455c44035c0de7aa5203e312a3ee178
0skar <0skar01@hotmail.se>
2024-10-16 14:35:08 +0200

    Add Images to construktor

--- src/main/java/com/example/pumpkin/model/Model.java
+++ src/main/java/com/example/pumpkin/model/Model.java
@@ -1,8 +1,37 @@
 package com.example.pumpkin.model;
 
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+import com.example.pumpkin.Pumpkin;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.image.Image;
 
 public class Model {
 
+    private int score = 0;
+    Image noSmash;
+    Image smash;
+
+
+    ObjectProperty<Image> firstPumpkin;
+    ObjectProperty<Image> secondPumpkin;
+    ObjectProperty<Image> thirdPumpkin;
+
+    public Model() {
+        noSmash = new Image(getClass().getResource("/com/example/pumpkin/images/pumpkin_blank.png").toExternalForm());
+        smash = new Image(getClass().getResource("/com/example/pumpkin/images/pumpkin1.png").toExternalForm());
+
+        firstPumpkin = new SimpleObjectProperty<>(noSmash);
+        secondPumpkin = new SimpleObjectProperty<>(noSmash);
+        thirdPumpkin = new SimpleObjectProperty<>(noSmash);
+    }
+
+
+
+
+
+    public void pumpkinSmashed(Pumpkin pumpkin) {
+        //check if we can smack this pumpkin
+        //Increase score
+
+    }
 }
