8ea230148df5a26252c41e24c5f22b69f6ea22d3
0skar <0skar01@hotmail.se>
2024-10-16 13:07:21 +0200

    commit before new branch

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module com.example.pumpkin {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
 
 
     opens com.example.pumpkin to javafx.fxml;
     exports com.example.pumpkin;
+    exports com.example.pumpkin.model;
+    opens com.example.pumpkin.model to javafx.fxml;
+    exports com.example.pumpkin.controller;
+    opens com.example.pumpkin.controller to javafx.fxml;
 }
